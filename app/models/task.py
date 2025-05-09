21b0932ee14a917daa42a53c2a8e9bca6b8e0353
Camille <camilledubois1995@gmail.com>
2025-05-09 12:23:31 -0400

    implemented Task model in task.py

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,23 @@
+from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at
+        }
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        task = cls(title=task_data["title"], description=task_data["description"], completed_at=task_data["completed_at"])
+        return task
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, Response, abort, make_response, request
+from app.models.task import Task
+
+
+bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
