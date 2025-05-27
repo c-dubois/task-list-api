9306b1286b38afdfcafc38a5b599425b62a54da7
Camille <camilledubois1995@gmail.com>
2025-05-27 16:40:11 -0700

    implemented get/put/delete by goal_id

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -34,3 +34,29 @@ def get_goals():
         goals_response.append(goal.to_dict())
 
     return goals_response
+
+@bp.get("/<goal_id>")
+def get_one_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    return {"goal": goal.to_dict()}
+
+@bp.put("<goal_id>")
+def update_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+
+    if "title" in request_body:
+        goal.title = request_body["title"]
+
+    db.session.commit()
+    return "", 204
+
+@bp.delete("/<goal_id>")
+def delete_task(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return "", 204
