a7e51265d6ff792357b52a76000234d74658ada9
Camille <camilledubois1995@gmail.com>
2025-05-27 16:24:32 -0700

    implemented Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,15 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title
+        }
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        task = cls(title=task_data["title"])
+        return task
