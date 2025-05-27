170e7436cc546c3c3769170413cfa1485344fb35
Camille <camilledubois1995@gmail.com>
2025-05-27 16:27:27 -0700

    registered goals bp in init.py

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,1 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, Response, abort, make_response, request
+from app.models.task import Task
+from app.routes.routes_utilities import validate_model, create_model
+from ..db import db
+
+bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
