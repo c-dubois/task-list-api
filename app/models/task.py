5a9e2b4ede9d75ffbbc6f1a400967927a7f075b8
Camille <camilledubois1995@gmail.com>
2025-05-27 17:22:23 -0700

    updated goal and task models to a one-to-many relationship

--- app/models/task.py
+++ app/models/task.py
@@ -1,23 +1,37 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime, ForeignKey
 from ..db import db
+from typing import Optional, TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="goals")
 
     def to_dict(self):
-        return {
+        task_dict = {
             "id": self.id,
             "title": self.title,
             "description": self.description,
             "is_complete": self.completed_at is not None
         }
+        if self.goal:
+            task_dict["goal"] = self.goal.title
+
+        return task_dict
+
     
     @classmethod
     def from_dict(cls, task_data):
-        task = cls(title=task_data["title"], description=task_data["description"], completed_at=task_data.get("completed_at"))
+        goal_id = task_data.get("goal_id")
+        task = cls(title=task_data["title"], 
+                description=task_data["description"], 
+                completed_at=task_data.get("completed_at"),
+                goal_id = goal_id)
         return task
